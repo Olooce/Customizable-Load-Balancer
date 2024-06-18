3c6ee9f0b0f88da9f301bb9b1d0e9c2f36256abc
Nathan Mbugua <78223048+some-casual-coder@users.noreply.github.com>
2024-06-18 21:49:26 +0300

    include comments in bash file

--- start_multiple.sh
+++ start_multiple.sh
@@ -2,15 +2,27 @@
 j=1
 for i in {1..100}
 do
+    # Define start and end dates based on the loop variables
     start="$j day ago";
     end="${i} day ago";
+    
+    # Increment j by 1
     j=$((j+1));
+
+    # Format the start and end dates
     start_date="date -d '${start}' '+%Y-%m-%d'";
     end_date="date -d '${end}' '+%Y-%m-%d'";
+
+    # Evaluate the date commands to get the actual dates
     ss=$(eval "$start_date");
     ee=$(eval "$end_date");
+
+    # Uncomment the following line to debug and see the dates being processed
     #echo  $ee,$ss,$start_date,$end_date
 
+    # Send a GET request to the local NEO endpoint with the start and end dates
     curl "http://localhost:5001/neo?start_date=$ss&&end_date=$ee" >> /dev/null
+    
+    # Pause for 5 seconds before the next iteration
     sleep 5
 done
